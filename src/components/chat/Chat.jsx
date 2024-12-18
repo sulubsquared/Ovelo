dfeaed40f83924711bbcbdd8044b74b77b03bd3a
sulubsquared <sulubsulub06@gmail.com>
2024-12-18 18:25:07 -0500

    Implemented chat details; img support

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -30,7 +30,57 @@ const Chat = () => {
                     <img src="./info.png" alt="" />
                 </div>
             </div>
-            <div className="center"></div>
+            <div className="center">
+                <div className="message">
+                    <img src="./avatar.jpg" alt="" />
+                    <div className="texts">
+                       <p>The quick brown fox jumps over the lazy dog. The quick 
+                        brown fox jumps over the lazy dog. The quick brown fox 
+                        jumps over the lazy dog.
+                        </p>
+                        <span>1 min ago</span> 
+                    </div>
+                </div>
+                <div className="message own">
+                    <div className="texts">
+                       <p>The quick brown fox jumps over the lazy dog. The quick 
+                        brown fox jumps over the lazy dog. The quick brown fox 
+                        jumps over the lazy dog.
+                        </p>
+                        <span>1 min ago</span> 
+                    </div>
+                </div>
+                <div className="message">
+                    <img src="./avatar.jpg" alt="" />
+                    <div className="texts">
+                       <p>The quick brown fox jumps over the lazy dog. The quick 
+                        brown fox jumps over the lazy dog. The quick brown fox 
+                        jumps over the lazy dog.
+                        </p>
+                        <span>1 min ago</span> 
+                    </div>
+                </div>
+                <div className="message own">
+                    <div className="texts">
+                        <img src="https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg" alt="" />
+                       <p>The quick brown fox jumps over the lazy dog. The quick 
+                        brown fox jumps over the lazy dog. The quick brown fox 
+                        jumps over the lazy dog.
+                        </p>
+                        <span>1 min ago</span> 
+                    </div>
+                </div>
+                <div className="message">
+                    <img src="./avatar.jpg" alt="" />
+                    <div className="texts">
+                       <p>The quick brown fox jumps over the lazy dog. The quick 
+                        brown fox jumps over the lazy dog. The quick brown fox 
+                        jumps over the lazy dog.
+                        </p>
+                        <span>1 min ago</span> 
+                    </div>
+                </div>
+            </div>
             <div className="bottom">
                 <div className="icons">
                     <img src="./img.png" alt="" />
--- src/components/detail/Detail.jsx
+++ src/components/detail/Detail.jsx
@@ -2,7 +2,88 @@ import "./detail.css"
 
 const Detail = () => {
     return (
-        <div className='detail'>Detail</div>
+        <div className='detail'>
+            <div className="user">
+                <img src="./avatar.jpg" alt="" />
+                <h2>Jane Doe</h2>
+                <p>The quick brown fox jumps.</p>
+            </div>
+            <div className="info">
+                <div className="option">
+                    <div className="title">
+                        <span>Chat Settings</span>
+                        <img src="./arrowUp.png" alt="" />
+                    </div>
+                </div>
+                <div className="option">
+                    <div className="title">
+                        <span>Chat Settings</span>
+                        <img src="./arrowUp.png" alt="" />
+                    </div>
+                </div>
+                <div className="option">
+                    <div className="title">
+                        <span>Privacy and Help</span>
+                        <img src="./arrowUp.png" alt="" />
+                    </div>
+                </div>
+                <div className="option">
+                    <div className="title">
+                        <span>Shared Photos</span>
+                        <img src="./arrowDown.png" alt="" />
+                    </div>
+                    <div className="photos">
+                        <div className="photoItem">
+                            <div className="photoDetail">
+                            <img 
+                            src="https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg" 
+                            alt="" 
+                            />
+                            <span>IMG_1012.png</span>
+                            </div>
+                            <img src="./download.png" alt="" className="icon" />
+                        </div>
+                        <div className="photoItem">
+                            <div className="photoDetail">
+                            <img 
+                            src="https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg" 
+                            alt="" 
+                            />
+                            <span>IMG_1012.png</span>
+                            </div>
+                            <img src="./download.png" alt="" className="icon" />
+                        </div>
+                        <div className="photoItem">
+                            <div className="photoDetail">
+                            <img 
+                            src="https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg" 
+                            alt="" 
+                            />
+                            <span>IMG_1012.png</span>
+                            </div>
+                            <img src="./download.png" alt="" className="icon" />
+                        </div>
+                        <div className="photoItem">
+                            <div className="photoDetail">
+                            <img 
+                            src="https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg" 
+                            alt="" 
+                            />
+                            <span>IMG_1012.png</span>
+                            </div>
+                            <img src="./download.png" alt="" className="icon" />
+                        </div>
+                    </div>
+                </div>
+                <div className="option">
+                    <div className="title">
+                        <span>Shares Files</span>
+                        <img src="./arrowUp.png" alt="" />
+                    </div>
+                </div>
+                <button>Block User</button>
+            </div>
+        </div>
     )
 }
 
