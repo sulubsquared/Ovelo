2198e05ad912260d2c5b656b414bd31fb1d36de5
sulubsquared <sulubsulub06@gmail.com>
2024-12-23 23:01:18 -0500

    scroll to latest text on open

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -1,10 +1,16 @@
-import { useState } from "react"
+import { useEffect, useState, useRef } from "react";
 import "./chat.css"
 import EmojiPicker from "emoji-picker-react"
 
 const Chat = () => {
-    const [open, setOpen] = useState(false)
-    const [text, setText] = useState("")
+    const [open, setOpen] = useState(false);
+    const [text, setText] = useState("");
+
+    const endRef = useRef(null)
+
+    useEffect(() => {
+        endRef.current?.scrollIntoView({ behavior: "smooth" });
+    }, []);
 
     const handleEmoji = e =>{
         setText((prev) => prev + e.emoji);
@@ -80,6 +86,7 @@ const Chat = () => {
                         <span>1 min ago</span> 
                     </div>
                 </div>
+                <div ref={endRef}></div>
             </div>
             <div className="bottom">
                 <div className="icons">
