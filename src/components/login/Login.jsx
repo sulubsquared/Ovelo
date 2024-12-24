6f963f93fe14951ece9b558f1c0c44d753aef014
sulubsquared <sulubsulub06@gmail.com>
2024-12-23 23:03:11 -0500

    new login/register page

---
+++ src/components/login/Login.jsx
@@ -0,0 +1,50 @@
+import { toast } from "react-toastify";
+import "./login.css";
+import React, { useState } from 'react';
+
+const Login = () => {
+    const [avatar,setAvatar] = useState({
+        file:null,
+        url: ""
+    })
+
+    const handleAvatar = e =>{
+        if(e.target.files[0]){
+        setAvatar({
+            file:e.target.files[0],
+            url: URL.createObjectURL(e.target.files[0])
+        })
+        }
+    }
+
+    const handleLogin = e =>{
+        e.preventDefault()
+    }
+
+    return  <div className="login">
+        <div className="item">
+        <h2>Welcome back,</h2>
+        <form onSubmit={handleLogin}>
+            <input type="text" placeholder="Email" name="email" />
+            <input type="password" placeholder="Password" name="password" />
+            <button>Log In</button>
+        </form>
+        </div>
+        <div className="separator"></div> 
+        <div className="item">
+        <h2>Create an Account</h2>
+        <form>
+            <label htmlFor="file">
+                <img src={avatar.url || "./avatar.jpg"} alt="" />
+                Upload an Image</label>
+            <input type="file" id="file" style={{display:"none"}} onChange={handleAvatar}/>
+            <input type="text" placeholder="Username" name="username" />
+            <input type="text" placeholder="Email" name="email" />
+            <input type="password" placeholder="Password" name="password" />
+            <button>Register</button>
+        </form>
+        </div> 
+    </div>
+};
+
+export default Login;
