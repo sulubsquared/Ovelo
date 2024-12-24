bcc8d88e098885487863d6b086efa7dc4f29047d
sulubsquared <sulubsulub06@gmail.com>
2024-12-23 23:02:51 -0500

    implement subcomponent

--- src/components/list/chatList/ChatList.jsx
+++ src/components/list/chatList/ChatList.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react"
 import "./chatList.css"
+import AddUser from "./addUser/addUser"
 
 const ChatList = () => {
     const [addMode,setAddMode]= useState(false)
@@ -94,6 +95,7 @@ const ChatList = () => {
                     <p>Hello</p>
                 </div>
             </div>
+            {addMode && <AddUser/>}
         </div>
     )
 }
